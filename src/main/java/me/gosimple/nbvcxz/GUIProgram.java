525d968334599c2e9dab7c9ebe58be0408c180a6
Taeyun Kim <taeyun1010@gmail.com>
2018-01-08 17:09:10 +0900

    finds files in myDocuments folder

--- src/main/java/me/gosimple/nbvcxz/GUIProgram.java
+++ src/main/java/me/gosimple/nbvcxz/GUIProgram.java
@@ -4,14 +4,25 @@ import java.awt.*;
 import java.awt.event.*; // Using AWT event classes and listener interfaces
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.io.RandomAccessRead;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
 
 import me.gosimple.nbvcxz.resources.Dictionary;
 import me.gosimple.nbvcxz.resources.DictionaryUtil;
 import me.gosimple.nbvcxz.resources.Generator;
-
 
 //An AWT GUI program inherits the top-level container java.awt.Frame
 public class GUIProgram extends Frame implements ActionListener, WindowListener {
@@ -26,19 +37,19 @@ public class GUIProgram extends Frame implements ActionListener, WindowListener 
 	// Constructor to setup the GUI components and event handlers
 	public GUIProgram(Nbvcxz nbvcxz2) {
 		nbvcxz = nbvcxz2;
-		
+
 		setLayout(new FlowLayout()); // "super" Frame sets to FlowLayout
 
 		add(new Label("Username  ")); // "super" Frame adds an anonymous Label
 		tfCount1 = new TextField("", 10); // Construct the TextField
-		tfCount1.setEditable(true); 
+		tfCount1.setEditable(true);
 		add(tfCount1); // "super" Frame adds TextField
-		
+
 		add(new Label("Password")); // "super" Frame adds an anonymous Label
 		tfCount2 = new TextField("", 10); // Construct the TextField
-		tfCount2.setEditable(true); 
+		tfCount2.setEditable(true);
 		add(tfCount2); // "super" Frame adds TextField
-		
+
 		add(new Label("Revised Password")); // "super" Frame adds an anonymous Label
 		tfCount3 = new TextField("", 30); // Construct the TextField
 		tfCount3.setEditable(false); // read-only
@@ -64,66 +75,104 @@ public class GUIProgram extends Frame implements ActionListener, WindowListener 
 	@Override
 	public void actionPerformed(ActionEvent evt) {
 		String originalpw = tfCount2.getText();
-		//tfCount3.setText(originalpw);
-		
-		//get path to documents folder
-		 String myDocuments = null;
+		// tfCount3.setText(originalpw);
 
-		 try {
-		     Process p =  Runtime.getRuntime().exec("reg query \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders\" /v personal");
-		     p.waitFor();
+		// get path to documents folder
+		String myDocuments = null;
 
-		     InputStream in = p.getInputStream();
-		     byte[] b = new byte[in.available()];
-		     in.read(b);
-		     in.close();
+		try {
+			Process p = Runtime.getRuntime().exec(
+					"reg query \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders\" /v personal");
+			p.waitFor();
 
-		     myDocuments = new String(b);
-		     myDocuments = myDocuments.split("\\s\\s+")[4];
+			InputStream in = p.getInputStream();
+			byte[] b = new byte[in.available()];
+			in.read(b);
+			in.close();
 
-		 } catch(Throwable t) {
-		     t.printStackTrace();
-		 }
+			myDocuments = new String(b);
+			myDocuments = myDocuments.split("\\s\\s+")[4];
 
-		 System.out.println(myDocuments);
-		
-		
-		
-		Dictionary dic =  new Dictionary("userdata", DictionaryUtil.loadUnrankedDictionary("userdata.txt"), false);
-		String revisedpw = Generator.generatePassphrase("l", 3, dic);
-		 tfCount3.setText(revisedpw);
-		 
-		
-		
-//		InputStream testInput;
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+
+//		//find all files in myDocuments folder
+//		File dir = new File(myDocuments);
+//		Collection<File> allfiles = FileUtils.listFiles(dir, null, true);
+//		Collection<File> pdffiles = FileUtils.listFiles(dir, new String[] { "pdf" }, true);
+
 //		try {
-//			// Create a stream to hold the output
-//		    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//		    PrintStream ps = new PrintStream(baos);
-//		    // IMPORTANT: Save the old System.out!
-//		    PrintStream old = System.out;
-//		    // Tell Java to use your special stream
-//		    System.setOut(ps);
-//			
-//			testInput = new ByteArrayInputStream("g".getBytes("UTF-8"));
-//			System.setIn( testInput );
-//			
-//		   
-//		    // Put things back
-//		    System.out.flush();
-//		    System.setOut(old);
-//		    // Show what happened
-//		    String revisedpw = baos.toString();
-//		    tfCount3.setText(revisedpw);
-//		    
-//		} catch (UnsupportedEncodingException e) {
+//			extractpdf(new File("C:\\Users\\User\\Documents\\2017_2_test.pdf"));
+//		} catch (IOException e) {
 //			// TODO Auto-generated catch block
 //			e.printStackTrace();
 //		}
-//		InputStream old = System.in;
-//		
-//		System.setIn( old );
 		
+//		pdffiles.forEach(file -> {
+//			try {
+//				extractpdf(file);
+//			}  catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		});
+
+		Dictionary dic = new Dictionary("userdata", DictionaryUtil.loadUnrankedDictionary("userdata.txt"), false);
+		String revisedpw = Generator.generatePassphrase("l", 3, dic);
+		tfCount3.setText(revisedpw);
+
+		// InputStream testInput;
+		// try {
+		// // Create a stream to hold the output
+		// ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		// PrintStream ps = new PrintStream(baos);
+		// // IMPORTANT: Save the old System.out!
+		// PrintStream old = System.out;
+		// // Tell Java to use your special stream
+		// System.setOut(ps);
+		//
+		// testInput = new ByteArrayInputStream("g".getBytes("UTF-8"));
+		// System.setIn( testInput );
+		//
+		//
+		// // Put things back
+		// System.out.flush();
+		// System.setOut(old);
+		// // Show what happened
+		// String revisedpw = baos.toString();
+		// tfCount3.setText(revisedpw);
+		//
+		// } catch (UnsupportedEncodingException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		// InputStream old = System.in;
+		//
+		// System.setIn( old );
+
+	}
+
+	public void extractpdf(File file) throws IOException {
+		
+		
+		FileInputStream fis = new FileInputStream(file);
+		PDFParser parser = new PDFParser(fis);
+		
+		parser.parse();
+		
+		COSDocument cosDoc = parser.getDocument();
+		
+		PDDocument pdDoc = new PDDocument(cosDoc);
+		
+		PDFTextStripper strip = new PDFTextStripper();
+		
+		String data = strip.getText(pdDoc);
+		
+		System.out.println(data);
+		
+		
+	      pdDoc.close();
 	}
 
 	/* WindowEvent handlers */
